5815486d5ef1d8118c840c87182f68b246ec4908
김기현 <creeps1104@gmail.com>
2023-10-31 16:53:13 +0900

    [FIX] pod file 추가, conflict 해결과정에서의 오류 해결 , 중복되는 함수 , 쓰이지않는 메서드 제거

--- DearMyFriend/DearMyFriend/ViewController/Entry/Main/Menu/MapViewController.swift
+++ DearMyFriend/DearMyFriend/ViewController/Entry/Main/Menu/MapViewController.swift
@@ -6,7 +6,6 @@ import SnapKit
 
 class MapViewController: UIViewController {
     var selectedItem: Item?
-
     var locationManager = CLLocationManager()
     var naverMapView: NMFNaverMapView?
     let geocoder = CLGeocoder()
@@ -14,7 +13,6 @@ class MapViewController: UIViewController {
     let naverSearch = MoyaProvider<NaverSearchService>()
     let SearchResultCell = "SearchResultCell"
     var markers: [NMFMarker] = []
-
     var searchResultsTableView: UITableView = UITableView()
     var searchController: UISearchController!
     var searchResultsTableViewController: UITableViewController!
@@ -137,14 +135,6 @@ class MapViewController: UIViewController {
 
 // MARK: - CLLocationManagerDelegate Extension
 extension MapViewController: CLLocationManagerDelegate {
-
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        if manager.authorizationStatus == .authorizedWhenInUse {
-            manager.startUpdatingLocation()
-        } else {
-            print("위치 권한이 부여되지 않았습니다.")
-        }
-    }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             DispatchQueue.main.async {
@@ -161,14 +151,10 @@ extension MapViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
 
         if let searchText = searchBar.text {
-            geocoder.geocodeAddressString(searchText) { [weak self] placemarks, error in
-                if let placemark = placemarks?.first, let location = placemark.location {
-                    let coordinate = location.coordinate
-                    self?.addMarker(at: coordinate, title: searchText)
-                }
-            }
+            searchLocalPlaces(searchText)
         }
     }
+
     func addMarker(at coordinate: CLLocationCoordinate2D, title: String) {
         guard let mapView = naverMapView?.mapView else {
             return
@@ -182,66 +168,51 @@ extension MapViewController: UISearchBarDelegate {
         markers.append(marker)
     }
 }
+
 extension MapViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchResults.count + recentSearches.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath)
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: SearchResultCell)
 
         if indexPath.row < recentSearches.count {
             cell.textLabel?.text = recentSearches[indexPath.row]
         } else {
-            cell.textLabel?.text = searchResults[indexPath.row - recentSearches.count]
-            if let item = searchResults[indexPath.row - recentSearches.count] as? Item {
-                cell.textLabel?.text = item.title
-                cell.detailTextLabel?.text = item.roadAddress // 로드 어드레스 추가
-            }
+            let resultIndex = indexPath.row - recentSearches.count
+            let resultItem = searchResults[resultIndex]
+            cell.textLabel?.text = resultItem.title
+
+            // Add road address to detailTextLabel
+            cell.detailTextLabel?.text = resultItem.roadAddress
         }
+
         return cell
     }
-    func distanceToLocation(_ mapy: String, _ mapx: String) -> String {
-        guard let currentLatitude = locationManager.location?.coordinate.latitude,
-              let currentLongitude = locationManager.location?.coordinate.longitude,
-              let targetLatitude = Double(mapy),
-              let targetLongitude = Double(mapx) else {
-            return "알 수 없음"
-        }
 
-        let currentLocation = CLLocation(latitude: currentLatitude, longitude: currentLongitude)
-        let targetLocation = CLLocation(latitude: targetLatitude, longitude: targetLongitude)
-
-        let distanceInMeters = currentLocation.distance(from: targetLocation)
-        return String(format: "%.2f", distanceInMeters)
-
-    }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("###:)",searchResults)
-
-
         if indexPath.row < recentSearches.count {
             let selectedSearch = recentSearches[indexPath.row]
             geocoder.geocodeAddressString(selectedSearch) { [weak self] placemarks, error in
                 if let placemark = placemarks?.first, let location = placemark.location {
-                    let coordinate = location.coordinate
-//                    self?.moveMapToLocation(coordinate)
-//                    self?.searchResultsTableView.isHidden = true
-                    print("###:)",selectedSearch)
-                }
-                    self?.moveMapToLocation(coordinate)
-                    self?.searchResultsTableView.isHidden = true
-                    print("###:)",selectedSearch)
+                    self?.moveMapToLocation(location.coordinate)
+                    self?.addMarker(at: location.coordinate, title: selectedSearch)
                 }
             }
         } else {
-            let selectedItem = searchResults[indexPath.row - recentSearches.count]
-            if let item = selectedItem as? Item, let roadAddress = item.roadAddress {
-                searchLocationWithAddress(roadAddress)
+            let resultIndex = indexPath.row - recentSearches.count
+            let selectedResultItem = searchResults[resultIndex]
+
+            geocoder.geocodeAddressString(selectedResultItem.roadAddress) { [weak self] placemarks, error in
+                if let placemark = placemarks?.first, let location = placemark.location {
+                    self?.moveMapToLocation(location.coordinate)
+                    self?.addMarker(at: location.coordinate, title: selectedResultItem.title)
+                }
             }
         }
-        
     }
+
     func moveMapToLocation(_ coordinate: CLLocationCoordinate2D) {
         print("Moving to coordinate: \(coordinate.latitude), \(coordinate.longitude)")
 
@@ -250,162 +221,46 @@ extension MapViewController: UITableViewDelegate, UITableViewDataSource {
             mapView.moveCamera(cameraUpdate)
         }
 
-        if let selectedItem = selectedItem {
-            let cleanTitle = selectedItem.cleanTitle()
-            addMarker(at: coordinate, title: cleanTitle)
-        }
-
         if !searchResults.isEmpty {
             searchResultsTableView.isHidden = true
         }
-
     }
 }
+
 extension MapViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-    @objc private func delayedSearch(_ searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text, !searchText.isEmpty else { return }
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(delayedSearch(_:)), object: searchController)
+        perform(#selector(delayedSearch(_:)), with: searchController, afterDelay: 0.2)
+    }
+
+    @objc func delayedSearch(_ searchController: UISearchController) {
         if let searchText = searchController.searchBar.text {
             searchLocalPlaces(searchText)
         }
     }
 }
 
-    // MARK: - CLLocationManagerDelegate Extension
-    extension MapViewController: CLLocationManagerDelegate {
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            if let location = locations.last {
-                DispatchQueue.main.async {
-                    print("현재 위치: \(location.coordinate.latitude), \(location.coordinate.longitude)")
-                    self.addMarker(at: location.coordinate, title: "현재위치요~")
-                }
-            }
-        }
-    }
-
-    // MARK: - UISearchBarDelegate Extension
-    extension MapViewController: UISearchBarDelegate {
-        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-            searchBar.resignFirstResponder()
-
-            if let searchText = searchBar.text {
-                searchLocalPlaces(searchText)
-            }
-        }
-
-        func addMarker(at coordinate: CLLocationCoordinate2D, title: String) {
-            guard let mapView = naverMapView?.mapView else {
-                return
-            }
-
-            let marker = NMFMarker()
-            marker.position = NMGLatLng(lat: coordinate.latitude, lng: coordinate.longitude)
-            marker.mapView = mapView
-            marker.captionText = title
-
-            markers.append(marker)
-        }
-    }
-
-    extension MapViewController: UITableViewDelegate, UITableViewDataSource {
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return searchResults.count + recentSearches.count
-        }
-
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: SearchResultCell)
-
-                   if indexPath.row < recentSearches.count {
-                       cell.textLabel?.text = recentSearches[indexPath.row]
-                   } else {
-                       let resultIndex = indexPath.row - recentSearches.count
-                       let resultItem = searchResults[resultIndex]
-                       cell.textLabel?.text = resultItem.title
-
-                       // Add road address to detailTextLabel
-                       cell.detailTextLabel?.text = resultItem.roadAddress
-                   }
-
-                   return cell
-               }
-
-
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            if indexPath.row < recentSearches.count {
-                let selectedSearch = recentSearches[indexPath.row]
-                geocoder.geocodeAddressString(selectedSearch) { [weak self] placemarks, error in
-                    if let placemark = placemarks?.first, let location = placemark.location {
-                        self?.moveMapToLocation(location.coordinate)
-                        self?.addMarker(at: location.coordinate, title:selectedSearch )
-                    }
-                }
-            } else {
-                let resultIndex = indexPath.row - recentSearches.count
-                let selectedResultItem  = searchResults[resultIndex]
-
-
-                geocoder.geocodeAddressString(selectedResultItem.roadAddress) { [weak self] placemarks, error in
-                    if let placemark = placemarks?.first, let location = placemark.location {
-                        self?.moveMapToLocation(location.coordinate)
-                        self?.addMarker(at: location.coordinate, title:selectedResultItem.title )
-                    }
-                }
-            }
-        }
-
-        func moveMapToLocation(_ coordinate: CLLocationCoordinate2D) {
-            print("Moving to coordinate: \(coordinate.latitude), \(coordinate.longitude)")
-
-            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(from: coordinate))
-            if let mapView = naverMapView?.mapView {
-                mapView.moveCamera(cameraUpdate)
-            }
-
-            if let selectedItem = selectedItem {
-                let cleanTitle = selectedItem.cleanTitle()
-                addMarker(at: coordinate, title: cleanTitle)
-            }
-
-            //        if !searchResults.isEmpty {
-            searchResultsTableView.isHidden = true
-        }
-    }
-
-    extension MapViewController: UISearchResultsUpdating {
-        func updateSearchResults(for searchController: UISearchController) {
-            guard let searchText = searchController.searchBar.text,
-                  !searchText.isEmpty else { return }
-
-            NSObject.cancelPreviousPerformRequests(withTarget: self,
-                                                   selector: #selector(delayedSearch(_:)),
-                                                   object: searchController)
-
-            perform(#selector(delayedSearch(_:)), with: searchController, afterDelay: 0.2)
-        }
-
-
-
-        func searchLocalPlaces(_ query: String) {
-            naverSearch.request(.search(query: query)) { [weak self] result in
-                switch result {
+extension MapViewController {
+    func searchLocalPlaces(_ query: String) {
+        naverSearch.request(.search(query: query)) { [weak self] result in
+            switch result {
                 case .success(let response):
                     do {
                         let decoder = JSONDecoder()
                         let results = try decoder.decode(Welcome.self, from: response.data)
                         var searchResultsWithAddresses: [(title: String, roadAddress: String)] = []
 
-
                         if let mapView = self?.naverMapView?.mapView {
                             for item in results.items {
                                 let title = item.title
                                 let phoneNumber = item.telephone
-
                                 var roadAddress: String? = item.roadAddress
 
                                 if let roadAddress = roadAddress {
                                     print("Title: \(title), roadAddress: \(roadAddress), phoneNumber: \(phoneNumber)")
-
-                                    self?.searchLocationWithAddress(roadAddress)
-                                    self?.selectedItem = item
+                                    // self?.searchLocationWithAddress(roadAddress)
+                                    // self?.selectedItem = item
                                 } else {
                                     let alertController = UIAlertController(title: "주소를 찾을 수 없습니다", message: "해당 장소의 주소 정보를 찾을 수 없습니다.", preferredStyle: .alert)
                                     let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
@@ -424,21 +279,21 @@ extension MapViewController: UISearchResultsUpdating {
                     }
                 case .failure(let error):
                     print("Network request error: \(error)")
-                }
             }
         }
     }
+}
 
-    extension MapViewController {
-        func searchLocationWithAddress(_ address: String) {
-            print("###:)", address)
-            geocoder.geocodeAddressString(address) { [weak self] placemarks, error in
-                if let placemark = placemarks?.first, let location = placemark.location {
-                    let coordinate = location.coordinate
-                    self?.moveMapToLocation(coordinate)
-                } else {
-                    print("주소를 찿을수 없어예")
-                }
+extension MapViewController {
+    func searchLocationWithAddress(_ address: String) {
+        print("###:)", address)
+        geocoder.geocodeAddressString(address) { [weak self] placemarks, error in
+            if let placemark = placemarks?.first, let location = placemark.location {
+                let coordinate = location.coordinate
+                self?.moveMapToLocation(coordinate)
+            } else {
+                print("주소를 찿을수 없어예")
             }
         }
     }
+}
